78bfde57db4878555935b30d7f86193a02cd0801
Alvaro Japa Salazar <alvaruwiiu@hotmail.com>
2023-04-19 23:37:49 -0500

    avance login

--- src/Pages/Intranet/Login/Login.jsx
+++ src/Pages/Intranet/Login/Login.jsx
@@ -1,18 +1,32 @@
 import React from 'react'
 import portada from './../../../assets/carril/about_1.png'
 import logo from './../../../assets/logo/logo.png'
+import './Login.css'
 const Login = () => {
     return (
         <>
-            <aside>
-                <img src={portada} alt="" />
-            </aside>
-            <aside>
-                <div>
-                <i class='bx bx-chevron-left'></i>
-                <img src={logo} alt="" />
+
+            <div class="login-container">
+                <div class="login-left">
+                    <img src={portada} alt="Imagen de login" />
                 </div>
-            </aside>
+                <div class="login-right">
+                    <h2>INTRANET</h2>
+                    <p>Solo para agentes asociados a Perú Exploring</p>
+                    <form>
+                        <label for="username">Usuario:</label>
+                        <input type="text" id="username" name="username" />
+                        <label for="password">Contraseña:</label>
+                        <input type="password" id="password" name="password" />
+
+                        <button type="submit">Iniciar sesión</button>
+
+                    </form>
+                    <p className='login-right-create'>Quieres ser un Agente? <span>Rellene este formulario de Inscripción</span></p>
+                </div>
+            </div>
+
+
         </>
     )
 }
